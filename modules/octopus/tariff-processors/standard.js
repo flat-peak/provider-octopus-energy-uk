7045e37eb05dce7349748087538b99d791f1549d
Alert1437 <a.a.cherkaev@gmail.com>
2023-01-12 10:23:18 +0200

    Add StandardTariff support

---
+++ modules/octopus/tariff-processors/standard.js
@@ -0,0 +1,35 @@
+const processStandardTariff = (octopusTariff) => {
+  return {
+    // FIXME: temporary set time_expiry as next midnight. Proper solution is needed.
+    'time_expiry': new Date(new Date().setHours(24, 0, 0, 0)).toISOString(),
+    'import': [
+      {
+        'type': 'weekday',
+        'data': [
+          {
+            'months': [
+              'All',
+            ],
+            'days_and_hours': [
+              {
+                'days': [
+                  'All',
+                ],
+                'hours': [
+                  {
+                    'valid_from': '00:00:00',
+                    'valid_to': '00:00:00',
+                    'cost': octopusTariff.unitRate,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+};
+
+
+module.exports = {processStandardTariff};
